f293499aef56206eb99c9e5c424ef882fe8b9621
Paudse <b92209003@ntu.edu.tw>
2025-11-17 17:04:54 -0800

    .

--- flask_app.py
+++ flask_app.py
@@ -26,43 +26,35 @@ def read_file():
 	with open(foler_file_name, "r", encoding='utf-8') as f: 
 		data = f.readlines()
 
-	de = []
-	ch = []
-	item_line_num = 3
+	t1 = []
+	t2 = []
+	t3 = []
+	item_line_num = 4
 	for i in range (0,len(data)):
 		if i%item_line_num == 0:
-			de.append(data[i].replace('\n', ''))
+			t1.append(data[i].replace('\n', ''))
 		if i%item_line_num == 1:
-			ch.append(data[i].replace('\n', ''))
+			t2.append(data[i].replace('\n', ''))
+		if i%item_line_num == 2:
+			t3.append(data[i].replace('\n', ''))
 
 
 
-	c = list(zip(de, ch))
+	c = list(zip(t1, t2, t3))
 	random.shuffle(c)
-	de, ch = zip(*c)
-	print(colored(de[0], 'yellow', attrs=['bold']))
-	print('卦意:', ch[0])
-	return [de[0], ch[0]]
-
-
-
-
+	t1, t2, t3 = zip(*c)
+	print(colored(t1[0], 'yellow', attrs=['bold']))
+	print('卦象:', t2[0])
+	print('卦意:', t3[0])
+	return [t1[0], t2[0], t3[0]]
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    if "index" not in session:
-        session["index"] = 0  # 初始化
-
-    if request.method == "POST":
-        # texts = read_file()
-        # 按下按鈕，切換到下一段文字
-        session["index"] += 1
-        texts = read_file()
-
     texts = read_file()
     current_text1 = texts[0]
     current_text2 = texts[1]
-    return render_template("index.html", text1=current_text1, text2=current_text2)
+    current_text3 = texts[2]
+    return render_template("index.html", text1=current_text1, text2=current_text2, text3=current_text3)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
